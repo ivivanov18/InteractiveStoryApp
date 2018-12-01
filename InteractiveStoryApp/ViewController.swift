bc693e45e624605126136a55b77de6a9ab690ad7
ivivanov18 <ivanov.ivan.v@gmail.com>
2018-12-01 14:15:50 +0200

    Initial Commit

---
+++ InteractiveStoryApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InteractiveStoryApp
+//
+//  Created by ivan ivanov on 01/12/2018.
+//  Copyright © 2018 ivan ivanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
