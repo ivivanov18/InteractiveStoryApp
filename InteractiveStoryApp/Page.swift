b098213a0321f22d7123390d7eed07358cae109c
ivivanov18 <ivanov.ivan.v@gmail.com>
2018-12-18 23:49:58 +0200

    feat: added pagecontroller, segue from viewcontroller to pagecontroller
    set up interface on pagecontroller programmatically

---
+++ InteractiveStoryApp/Page.swift
@@ -0,0 +1,77 @@
+//
+//  Page.swift
+//  InteractiveStoryApp
+//
+//  Created by ivan ivanov on 01/12/2018.
+//  Copyright © 2018 ivan ivanov. All rights reserved.
+//
+
+import Foundation
+
+// STRUCTS GET MEMBER-WISE INITIALIZERS SO THERE IS NOT NEED FOR ONE
+
+//TYPE ALIASES = ALTERNATE NAME FOR A TYPE
+class Page{
+    let story: Story
+    
+    typealias Choice = (title: String, page: Page)
+    
+    var firstChoice: Choice?
+    var secondChoice: Choice?
+    
+    init(story: Story){
+        self.story = story
+    }
+}
+
+extension Page {
+    func addChoiceWith(title: String, story: Story) -> Page {
+        let page = Page(story: story)
+        return addChoiceWith(title: title, page: page)
+    }
+    
+    
+    func addChoiceWith(title: String, page: Page) -> Page {
+        
+//        guard firstChoice != nil && secondChoice != nil else {return self}
+//
+//        if firstChoice == nil{
+//            firstChoice = (title, page)
+//        }else{
+//            secondChoice = (title, page)
+//        }
+        
+        //OPTIONALS ARE JUST ENUMS UNDER THE HOOD
+        switch (firstChoice, secondChoice) {
+        case (.some, .some):
+            return self
+        case (.none, .none), (.none, .some): firstChoice = (title, page)
+        case (.some, .none): secondChoice = (title, page)
+        }
+        return page
+    }
+}
+
+struct Adventure {
+    static var story: Page {
+        let returnTrip = Page(story: .returnTrip)
+        let touchdown = returnTrip.addChoiceWith(title: "Stop and Investigate", story: .touchDown)
+        let homeward = returnTrip.addChoiceWith(title: "Continue home to Earth", story: .homeward)
+        let rover = touchdown.addChoiceWith(title: "Explore the Rover", story: .rover)
+        let crate = touchdown.addChoiceWith(title: "Open the Crate", story: .crate)
+        
+        homeward.addChoiceWith(title: "Head back to Mars", page: touchdown)
+        let home = homeward.addChoiceWith(title: "Continue Home to Earth", story: .home)
+        
+        let cave = rover.addChoiceWith(title: "Explore the Coordinates", story: .cave)
+        rover.addChoiceWith(title: "Return to Earth", page: home)
+        
+        cave.addChoiceWith(title: "Continue towards faint light", story: .droid)
+        cave.addChoiceWith(title: "Refill the ship and explore the rover", page: rover)
+        
+        crate.addChoiceWith(title: "Explore the Rover", page: rover)
+        crate.addChoiceWith(title: "Use the key", story: .monster)
+        
+        return returnTrip
+    }
+}
